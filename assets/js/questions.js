a2728cf0d186f282cfe871145edafe4e35fa638d
Vasilika Schnitzer <wasilika@hotmail.com>
2023-12-19 15:34:34 +0100

    Create script file questions.js that contains all lists with quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,115 @@
+const quizQuestionsTech = [
+  {
+    question: "Tech question 1",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+  {
+    question: "Tech question 2",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+  {
+    question: "Tech question 3",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizQuestionsNature = [
+  {
+    question: "Nature question",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizQuestionsSports = [
+  {
+    question: "Sports question",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizQuestionsHistory = [
+  {
+    question: "History question",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizQuestionsGeography = [
+  {
+    question: "Geography question",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizQuestionsBooks = [
+  {
+    question: "Book question",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizQuestionsMusic = [
+  {
+    question: "Music question",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizQuestionsFood = [
+  {
+    question: "Food question",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizQuestionsGeneral = [
+  {
+    question: "General question",
+    0: "Correct answer",
+    1: "Answer b",
+    2: "Answer c",
+    3: "Answer d",
+  },
+];
+
+const quizCategories = [
+  quizQuestionsTech,
+  quizQuestionsNature,
+  quizQuestionsSports,
+  quizQuestionsHistory,
+  quizQuestionsGeography,
+  quizQuestionsBooks,
+  quizQuestionsMusic,
+  quizQuestionsFood,
+  quizQuestionsGeneral,
+];

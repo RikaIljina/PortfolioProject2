d83877ec216f522f2899f82754cfaad2c87bb1d5
Vasilika Schnitzer <wasilika@hotmail.com>
2023-12-21 19:02:09 +0100

    Add comments and docstrings

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,11 +65,10 @@ function validateNames(event) {
  */
 function initializePlayers() {
   // Initialize player objects with names
-
   player1.name = playerData.elements["p1"].value;
   player2.name = playerData.elements["p2"].value;
 
-  // Initial update of the player areas to show player names
+  // Initial update of the player areas to show player names and colors
   document.getElementById("p1-name").textContent = player1.name;
   document.getElementById("p2-name").textContent = player2.name;
 
@@ -90,7 +89,7 @@ function initializePlayers() {
  * Resets colors, scores and round count.
  * Calls updatePlayerArea()
  */
-function restartGame() {
+function continueGame() {
   // Hide end game screen
   document.getElementById("end-game-screen").style.display = "none";
   document.getElementById("modal").style.display = "none";
@@ -110,36 +109,51 @@ function restartGame() {
   }
 }
 
+/**
+ * Updates the areas to the left and right of the card area with current player data.
+ * Highlights the active player.
+ */
 function updatePlayerArea() {
   document.getElementById("p1-score").textContent = player1.score;
   document.getElementById("p2-score").textContent = player2.score;
 
   let activePlayer = document.getElementById(gameState.activePlayer.id);
   let inactivePlayer = document.getElementById(gameState.inactivePlayer.id);
-  
+
   // Highlight active player
   activePlayer.style.color = "red";
-  activePlayer.parentElement.parentElement.style.backgroundColor = gameState.activePlayer.color;
+  activePlayer.parentElement.parentElement.style.backgroundColor =
+    gameState.activePlayer.color;
   activePlayer.parentElement.parentElement.style.border = "0.5em solid yellow";
   inactivePlayer.style.color = "black";
-  inactivePlayer.parentElement.parentElement.style.backgroundColor = gameState.inactivePlayer.colorInactive;
+  inactivePlayer.parentElement.parentElement.style.backgroundColor =
+    gameState.inactivePlayer.colorInactive;
   inactivePlayer.parentElement.parentElement.style.border = "0.5em solid grey";
   return;
 }
 
+/**
+ * Called whenever a player clicks on a category card.
+ * Shows the question card and calls showQuestion().
+ */
 function cardClicked() {
+  // Make sure the game has started to prevent players from accidentally clicking on cards
   if (gameState.gameStarted === true) {
-    // Show question card with relevant data-id and wait for active player input
+    // Show the card that will be filled with the question and answers
     document.getElementById("modal").style.display = "block";
     document.getElementById("question-card").style.display = "flex";
+
+    // Make sure the answers are clickable
     for (answer of document.getElementsByClassName("answer")) {
       answer.style.pointerEvents = "auto";
     }
 
     showQuestion(this);
 
-    // Making an element unclickable: https://stackoverflow.com/questions/16492401/javascript-setting-pointer-events
+    // Make this category card unclickable (https://stackoverflow.com/questions/16492401/javascript-setting-pointer-events)
     this.style.pointerEvents = "none";
+
+    // Save this category card in a global variable for later use in processAnswer()
     usedCard = this;
 
     return;
@@ -148,20 +162,24 @@ function cardClicked() {
   }
 }
 
+/**
+ * Selects a question to show to the player and fills the question card with
+ * possible answers.
+ * Calls processAnswer() once an answer has been clicked.
+ */
 function showQuestion(activeCard) {
-  // Get preselected question for the data-id and wait for answer
+  // Get a random question corresponding to the data-id of the clicked category card
   let categoryIndex = activeCard.getAttribute("data-id");
   let category = quizCategories[categoryIndex];
   let activeQuestion = category[Math.floor(Math.random() * category.length)];
 
   document.getElementById("question").textContent = activeQuestion.question;
 
-  // on click: validate, hide question, deactivate card
+  // Shuffle the answers
   let answerKeys = [0, 1, 2, 3];
   for (answer of document.getElementsByClassName("answer")) {
-    // Select random index to shuffle the display order of the questions
+    // Select random index to shuffle the display order of the answers
     let randomIndex = Math.floor(Math.random() * answerKeys.length);
-    console.log(randomIndex);
     // Fill the quiz card with the answers from the quiz dictionary
     answer.textContent = activeQuestion[answerKeys[randomIndex]];
     // Remember the element with the correct answer which is always on index 0
@@ -171,39 +189,60 @@ function showQuestion(activeCard) {
     // Remove the selected index from the list to make sure no answers are being selected multiple times
     answerKeys.splice(randomIndex, 1);
 
+    // Listen for player clicking on an answer
     answer.addEventListener("click", processAnswer);
   }
 
   return;
 }
 
+/**
+ * Checks if the selected answer is correct.
+ * Updates the score and the category card colors accordingly.
+ * Calls nextRound() once the player clicks on Continue.
+ */
 function processAnswer() {
   // Check if player has clicked on the correct answer
   if (this.getAttribute("id") === gameState.correctAnswer) {
     gameState.activePlayer.score += 100;
+    // If correct:
+    // Highlight the chosen answer green
     this.style.backgroundColor = "green";
+    // Display the category card in the player's colors
     usedCard.style.backgroundColor = gameState.activePlayer.color;
   } else {
+    // If wrong:
+    // Highlight the incorrect answer red and the correct answer green
     this.style.backgroundColor = "red";
     document.getElementById(gameState.correctAnswer).style.backgroundColor =
       "green";
-      usedCard.style.backgroundColor = "grey";
+    // Display the category card in grey
+    usedCard.style.backgroundColor = "grey";
   }
 
+  // Make sure palyers can't click on other answers once an answer has been selected
   for (answer of document.getElementsByClassName("answer")) {
     answer.style.pointerEvents = "none";
     answer.removeEventListener("click", processAnswer);
   }
 
+  // Listen for player clicking on Continue 
   document.getElementById("close-card").addEventListener("click", nextRound);
 
   return;
 }
 
+/**
+ * Switches to the next player, increments the round.
+ * Calls endGame() once last round is reached.
+ */
 function nextRound() {
+  // Hide question card and deactivate its button
   document.getElementById("question-card").style.display = "none";
   document.getElementById("modal").style.display = "none";
   document.getElementById("close-card").removeEventListener("click", nextRound);
+
+  // Reset the colors of answers on the question card
   for (answer of document.getElementsByClassName("answer")) {
     answer.style.backgroundColor = "";
   }
@@ -215,9 +254,10 @@ function nextRound() {
     gameState.inactivePlayer === player2 ? player1 : player2;
   // Increment current round by 1
   gameState.currentRound += 1;
+
   updatePlayerArea();
 
-  // If currentRound == 10, go to endGame
+  // Check if current round is last round and call endGame()
   if (gameState.currentRound === 10) {
     endGame();
   }
@@ -225,10 +265,16 @@ function nextRound() {
   return;
 }
 
+/**
+ * Processes the end of the game by displaying the results.
+ * Allows players to continue playing by calling continueGame() or to reset the game for new players by reloading the page.
+ */
 function endGame() {
   gameState.gameStarted = false;
   document.getElementById("end-game-screen").style.display = "block";
   document.getElementById("modal").style.display = "block";
+  
+  // Compare the player scores and update the end game screen accordingly
   if (player1.score === player2.score) {
     document.getElementById(
       "results"
@@ -244,9 +290,10 @@ function endGame() {
     "results"
   ).innerHTML += `<br>Total wins: <br> ${player1.name}: ${player1.wins} <br> ${player2.name}: ${player2.wins}`;
 
+  // Listen for players clicking on Continue or Restart
   document
     .getElementById("restart-same")
-    .addEventListener("click", restartGame);
+    .addEventListener("click", continueGame);
   document.getElementById("restart-new").addEventListener("click", () => {
     location.reload();
   });
